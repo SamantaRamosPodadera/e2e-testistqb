4468412707e8e32670a80177a29798b625966f35
Samanta1984 <samantaramos1984@hotmail.com>
2024-06-27 17:03:48 +0200

    correct answer

--- cypress/e2e/features/correct_answer.feature
+++ cypress/e2e/features/correct_answer.feature
@@ -4,6 +4,18 @@ Scenario: Respond with a correct answer
 
 Given the student is on the ISTQB exam page
 
-When the user selects "B" answer on question "1"
+When the user selects "b" answer on question "1"
 
+And the user press Correct on question "1"
 
+Then the user should see the correct message "The question is correct you receive 2" on question "1"
+
+Scenario: Respond with an incorrect answer
+
+Given the student is on the ISTQB exam page
+
+When the user selects "1" question on answer "a" 
+
+And the user press the Correct on question "1"
+
+Then the user should see the incorrect message "The question is incorrect you receive -1" on question "1"
--- cypress/e2e/step_definitions/correct_answer_steps.js
+++ cypress/e2e/step_definitions/correct_answer_steps.js
@@ -9,5 +9,29 @@ import {
 
 When('the user selects {string} answer on question {string}', (selectedAnswer,questionnumber) => {
 
-  cy.get('[data-cy="select-answer-1'+questionnumber+'"]').select(selectedAnswer);
+  cy.get('[data-cy="select-answer-'+questionnumber+'"]').select(selectedAnswer);
 })
+
+When('the user press Correct on question {string}', (questionnumber) => {
+  cy.get('[data-cy="button-correct-answer-'+questionnumber+'"]').click();
+})
+
+
+
+Then('the user should see the correct message {string} on question {string}', (correctAnswer, questionNumber) => {
+  cy.get('[data-cy="message-correct-asnwer-'+questionNumber+'"]').should('contain.text', correctAnswer);
+})
+
+When('the user selects {string} question on answer {string}', (questionnumber,selectedAnswer) => {
+
+  cy.get('[data-cy="select-answer-'+questionnumber+'"]').select(selectedAnswer);
+})
+
+When('the user press the Correct on question {string}', (questionnumber) => {
+  cy.get('[data-cy="button-correct-answer-'+questionnumber+'"]').click();
+})
+
+Then('the user should see the incorrect message {string} on question {string}', (icorrectAnswer, questionNumber) => {
+  cy.get('[data-cy="message-correct-asnwer-'+questionNumber+'"]').should('contain.text', icorrectAnswer);
+})
+
