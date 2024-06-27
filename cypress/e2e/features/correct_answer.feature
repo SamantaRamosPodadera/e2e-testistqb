751794c8aebf14064f02340ec9921e61e4e9c1af
Samanta1984 <samantaramos1984@hotmail.com>
2024-06-27 17:48:53 +0200

    correct-answer terminado

--- cypress/e2e/features/correct_answer.feature
+++ cypress/e2e/features/correct_answer.feature
@@ -1,21 +1,17 @@
 Feature: Correct answer
 
-Scenario: Respond with a correct answer
 
+Scenario Outline: Respond with an answer
 Given the student is on the ISTQB exam page
 
-When the user selects "b" answer on question "1"
+When the user selects <answer> answer on question <questionNumber>
 
-And the user press Correct on question "1"
+And the user press the Correct on question <questionNumber>
 
-Then the user should see the correct message "The question is correct you receive 2" on question "1"
+Then the user should see the message <message> on question <questionNumber>
 
-Scenario: Respond with an incorrect answer
-
-Given the student is on the ISTQB exam page
-
-When the user selects "1" question on answer "a" 
-
-And the user press the Correct on question "1"
-
-Then the user should see the incorrect message "The question is incorrect you receive -1" on question "1"
+Examples:
+| answer | questionNumber | message |
+| "b"    | "1" | "The question is correct you receive 2" |
+| "a"    | "1" | "The question is incorrect you receive -1" |
+| ""    | "1" | "The question is incorrect you receive 0" |
--- cypress/e2e/step_definitions/correct_answer_steps.js
+++ cypress/e2e/step_definitions/correct_answer_steps.js
@@ -12,26 +12,12 @@ When('the user selects {string} answer on question {string}', (selectedAnswer,qu
   cy.get('[data-cy="select-answer-'+questionnumber+'"]').select(selectedAnswer);
 })
 
-When('the user press Correct on question {string}', (questionnumber) => {
-  cy.get('[data-cy="button-correct-answer-'+questionnumber+'"]').click();
-})
-
-
-
-Then('the user should see the correct message {string} on question {string}', (correctAnswer, questionNumber) => {
-  cy.get('[data-cy="message-correct-asnwer-'+questionNumber+'"]').should('contain.text', correctAnswer);
-})
-
-When('the user selects {string} question on answer {string}', (questionnumber,selectedAnswer) => {
-
-  cy.get('[data-cy="select-answer-'+questionnumber+'"]').select(selectedAnswer);
-})
-
 When('the user press the Correct on question {string}', (questionnumber) => {
   cy.get('[data-cy="button-correct-answer-'+questionnumber+'"]').click();
 })
 
-Then('the user should see the incorrect message {string} on question {string}', (icorrectAnswer, questionNumber) => {
-  cy.get('[data-cy="message-correct-asnwer-'+questionNumber+'"]').should('contain.text', icorrectAnswer);
+
+Then('the user should see the message {string} on question {string}',  (message, questionNumber) => {
+  cy.get('[data-cy="message-correct-asnwer-'+questionNumber+'"]').should('contain.text', message);
 })
 
